6cdf85f78a0dbbfb85042212857a1b0267667ae8
sbx_user1051 <sbx_user1051@169.254.14.113>
2023-08-21 13:34:02 +0000

    Mon Aug 21 13:34:02 UTC 2023

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,7 +3,8 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name):
-    # ignoring arg friend_name, returning basic string to debug
-    return 'Hello, World!'
+    # saying hello to a friend :)
+    return f'Hello, {friend_name}!'
+
 
 
