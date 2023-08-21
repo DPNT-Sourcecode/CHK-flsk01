a58e35306c9eb143a35e7919eefeb38f8e37029f
sbx_user1051 <sbx_user1051@169.254.14.113>
2023-08-21 13:40:32 +0000

    Mon Aug 21 13:40:32 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -7,6 +7,16 @@ def checkout(skus):
     from the price table inc. offers, return the total cost of
     goods from a string of skus
     '''
+    # initial assumption is that skus is of the form 'ABCAD'
+    # therefore need to sum the characters in a string
+
+    # first, the basic price table as an object, no offers involved yet
+    prices = {
+        'A': 50,
+        'B': 30,
+        'C': 20,
+        'D': 15
+    }
     raise NotImplementedError()
 
 # Our price table and offers:
@@ -18,4 +28,5 @@ def checkout(skus):
 # | C    | 20    |                |
 # | D    | 15    |                |
 # +------+-------+----------------+
+
 
