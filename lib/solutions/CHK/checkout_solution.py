4a52e74139d8f74d2bebda25b9b1cb6a2d98c5b7
sbx_user1051 <sbx_user1051@169.254.14.113>
2023-08-21 15:27:08 +0000

    Mon Aug 21 15:27:08 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -55,7 +55,9 @@ def checkout(skus):
     }
 
     # add the T and Y items to S and remove as identical
-    
+    summed_items['S'] += summed_items['T'] + summed_items['Y']
+    summed_items['T'] == 0
+    summed_items['Y'] == 0
 
     # multiplying the price of each item with the count of each item
     total_cost = sum([prices[item]*count for item, count in summed_items.items()])
@@ -66,8 +68,14 @@ def checkout(skus):
     multi_buy_nums = sum(summed_items[item] for item in ['S','T','X','Y','Z'])
     if multi_buy_nums // 3 > 0:
         cost = (multi_buy_nums // 3)*45
-        if summed_items['Z'] > 0:
-            summed_items['Z'] = summed_items['Z']%3
+        if multi_buy_nums >= summed_items['Z']:
+            multi_buy_nums -= summed_items['Z']
+            summed_items['Z'] = 0
+        if multi_buy_nums >= summed_items['S']:
+            multi_buy_nums -= summed_items['S']
+            summed_items['S'] = 0
+        if multi_buy_nums >= summed_items['X']:
+
 
     # 5H for 45, 10H for 80 (for every 5, 5 off, for every 10, 20 off)
     if summed_items['H'] // 10 > 0:
@@ -135,6 +143,7 @@ def checkout(skus):
         total_cost -= (summed_items['B'] // 2)*15
 
     return total_cost
+
 
 
 
