ae0ace7513901ddc7f423e3de83818c8566d4f06
sbx_user1051 <sbx_user1051@169.254.14.113>
2023-08-21 13:37:32 +0000

    Mon Aug 21 13:37:32 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,19 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+    '''
+    from the price table inc. offers, return the total cost of
+    goods from a string of skus
+    '''
     raise NotImplementedError()
+
+# Our price table and offers:
+# +------+-------+----------------+
+# | Item | Price | Special offers |
+# +------+-------+----------------+
+# | A    | 50    | 3A for 130     |
+# | B    | 30    | 2B for 45      |
+# | C    | 20    |                |
+# | D    | 15    |                |
+# +------+-------+----------------+
+
